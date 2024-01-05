1216ba6b3fae304131d0a7a5e2c73d6a43c2647d
Abdurahman AYDIN <hariysu@gmail.com>
2024-01-05 16:40:25 +0300

    Initial Commit

---
+++ Flash-Chat-Section-15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flash-Chat-Section-15
+//
+//  Created by Abdurahman on 5.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
