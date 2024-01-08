5063fb671d72bff1e87808031616fc49d0269615
Abdurahman AYDIN <hariysu@gmail.com>
2024-01-08 17:13:02 +0300

    175-Typing animation, timer, for in loop

--- Flash-Chat-Section-15/Controllers/WelcomeViewController.swift
+++ Flash-Chat-Section-15/Controllers/WelcomeViewController.swift
@@ -14,7 +14,18 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        var charIndex = 0.0
+        titleLabel.text = ""
+        let titleText = "⚡️FlashChat"
+        for letter in titleText{
+            print("-")
+            print(0.1 * charIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1            
+        }
     }
     
 
