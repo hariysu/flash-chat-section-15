5c75bcb4900686e4fd0e51e24b985bcb93f288e2
Abdurahman AYDIN <hariysu@gmail.com>
2024-01-11 12:36:12 +0300

    Uses CLTypingLabel pod on project

--- Flash-Chat-Section-15/Controllers/WelcomeViewController.swift
+++ Flash-Chat-Section-15/Controllers/WelcomeViewController.swift
@@ -6,26 +6,15 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        var charIndex = 0.0
-        titleLabel.text = ""
-        let titleText = "⚡️FlashChat"
-        for letter in titleText{
-            print("-")
-            print(0.1 * charIndex)
-            print(letter)
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1            
-        }
+        titleLabel.text = "⚡️FlashChat"
     }
     
 
