11a2ecb26672083d00ee7a55b5a8bb7442ad11b7
Abdurahman AYDIN <hariysu@gmail.com>
2024-01-12 12:19:29 +0300

    187-Log out operations, navigationController.popToRootViewController, navigationItem.hidesBackButton, title

--- Flash-Chat-Section-15/Controllers/ChatViewController.swift
+++ Flash-Chat-Section-15/Controllers/ChatViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ChatViewController: UIViewController {
 
@@ -14,11 +15,20 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.hidesBackButton = true
+        title = "⚡️FlashChat"
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
     }
     
-
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        do {
+          try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+    }
+    
 }
