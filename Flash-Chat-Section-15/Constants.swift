dd529d2581ba6f95d408e48c1197bc5968086385
Abdurahman AYDIN <hariysu@gmail.com>
2024-01-12 17:32:04 +0300

    188-Constants, static (type property)

---
+++ Flash-Chat-Section-15/Constants.swift
@@ -0,0 +1,28 @@
+//
+//  Constants.swift
+//  Flash-Chat-Section-15
+//
+//  Created by Abdurahman on 12.01.2024.
+//
+
+struct K {
+    static let appName = "⚡️FlashChat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
