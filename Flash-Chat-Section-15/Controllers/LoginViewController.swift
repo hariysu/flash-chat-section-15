8c37742e828bf8e49c314a01a89e8fcb4fd4cd53
Abdurahman AYDIN <hariysu@gmail.com>
2024-01-12 11:50:36 +0300

    186-Logging in existing users

--- Flash-Chat-Section-15/Controllers/LoginViewController.swift
+++ Flash-Chat-Section-15/Controllers/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -14,6 +15,15 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text{
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                }else{
+                    // Navigate to the ChatViewController
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
     }
-    
 }
