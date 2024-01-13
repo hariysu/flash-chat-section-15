247f276ec632e2c74dacf917312dd68abd725345
Abdurahman AYDIN <hariysu@gmail.com>
2024-01-13 12:18:06 +0300

    189-UITableView, UITableViewDataSource, indexPath, UITableViewDelegate

--- Flash-Chat-Section-15/Controllers/ChatViewController.swift
+++ Flash-Chat-Section-15/Controllers/ChatViewController.swift
@@ -13,10 +13,14 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [Message(sender: "a@a.com", body: "Hey"), Message(sender: "b@b.com", body: "Hello"), Message(sender: "c@c.com", body: "What's up?")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
         title = K.appName
+        tableView.dataSource = self
+        //tableView.delegate = self
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -30,5 +34,22 @@ class ChatViewController: UIViewController {
           print("Error signing out: %@", signOutError)
         }
     }
-    
 }
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        return cell
+    }
+}
+
+//extension ChatViewController: UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(indexPath.row)
+//    }
+//}
---
+++ Flash-Chat-Section-15/Models/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash-Chat-Section-15
+//
+//  Created by Abdurahman on 13.01.2024.
+//
+
+import Foundation
+
+struct Message{
+    let sender: String
+    let body: String
+}
