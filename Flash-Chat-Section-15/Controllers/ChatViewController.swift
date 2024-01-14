c11a99a4b925518b0b0f002eb5c6682a38c71210
Abdurahman AYDIN <hariysu@gmail.com>
2024-01-14 12:54:54 +0300

    190-customising cells in a tableview using a .xib File - Xib file, nib, UINib

--- Flash-Chat-Section-15/Controllers/ChatViewController.swift
+++ Flash-Chat-Section-15/Controllers/ChatViewController.swift
@@ -21,6 +21,7 @@ class ChatViewController: UIViewController {
         title = K.appName
         tableView.dataSource = self
         //tableView.delegate = self
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -42,8 +43,8 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        cell.textLabel?.text = messages[indexPath.row].body
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
         return cell
     }
 }
